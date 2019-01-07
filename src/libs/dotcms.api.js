2bd862a188f33167813e434475d3a85c78e8d74e
Freddy Montes <freddymontes@gmail.com>
2019-01-07 13:50:44 -0600

    Use env variables in CRA and Node

--- src/libs/dotcms.api.js
+++ src/libs/dotcms.api.js
@@ -20,16 +20,18 @@ export default {
     processPage: processPage,
     getPage: async ({includeHost, pathname}) => {
         // TODO: we need to pass dinamically the lagunage_id and the host
-        const url = `${includeHost ? process.env.DEFAULT_HOST : ''}/api/v1/page/render/${pathname.slice(1)}?language_id=1`;
+        const url = `${includeHost ? process.env.REACT_APP_DEFAULT_HOST : ''}/api/v1/page/render/${pathname.slice(1)}?language_id=1`;
 
         return await fetch(url, {
             headers: {
-                DOTAUTH: Buffer.from(`${process.env.USER_EMAIL}:${process.env.USER_PASSWORD}`).toString('base64')
+                DOTAUTH: Buffer.from(`${process.env.REACT_APP_USER_EMAIL}:${process.env.REACT_APP_USER_PASSWORD}`).toString('base64')
             }
         })
             .then(data => data.json())
             .then(data => data.entity)
-            .then(page => processPage(page))
+            .then(page => {
+                return processPage(page);
+            })
             .catch(err => err);
     }
 };
