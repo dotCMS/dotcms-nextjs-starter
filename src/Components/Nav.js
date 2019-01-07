9b8d7b5a7cbcad21537aa24f55ae2c35229e7d96
Freddy Montes <freddymontes@gmail.com>
2019-01-07 16:35:42 -0600

    Basic components and fetch

---
+++ src/Components/Nav.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Nav as BootstrapNav, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
+
+export default class Nav extends Component {
+    state = {
+        isOpen: false,
+        items: []
+    };
+
+    componentDidMount() {
+        fetch('/api/v1/nav//?depth=2')
+            .then(response => response.json())
+            .then(data => data.entity.children)
+            .then(data => {
+                this.setState({
+                    ...this.state,
+                    items: data
+                });
+            });
+    }
+
+    toggleOpen = e => {
+        this.setState({
+            ...this.state,
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    render() {
+        return (
+            <>
+                <NavbarToggler onClick={this.toggleOpen} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <BootstrapNav pills="true">
+                        {this.state.items.map(item => {
+                            return (
+                                <NavItem>
+                                    <NavLink active={item.href === window.location.pathname} href={item.href}>
+                                        {item.title}
+                                    </NavLink>
+                                </NavItem>
+                            );
+                        })}
+                    </BootstrapNav>
+                </Collapse>
+            </>
+        );
+    }
+}
--- src/Page.js
+++ src/Page.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { Container as BootstrapContainer, Alert } from 'reactstrap';
 
 import RowContainer from './Components/RowContainer';
+import Header from './Components/Header';
 
 import './Page.css';
 
 const Page = ({ data }) => {
     return (
-        <BootstrapContainer>
-            {data && data.body && data.body.rows ? (
-                data.body.rows.map((row, i) => {
-                    return <RowContainer row={row} key={i} />;
-                })
-            ) : (
-                <Alert className="alert" color="danger">
-                    <h4 className="alert-heading">Page can't be rendered</h4>
-                    <p>You might not have enterprise license or the page is not created with our layout editor.</p>
-                </Alert>
-            )}
-        </BootstrapContainer>
+        <>
+            <Header />
+            <BootstrapContainer>
+                {data && data.body && data.body.rows ? (
+                    data.body.rows.map((row, i) => {
+                        return <RowContainer row={row} key={i} />;
+                    })
+                ) : (
+                    <Alert className="alert" color="danger">
+                        <h4 className="alert-heading">Page can't be rendered</h4>
+                        <p>You might not have enterprise license or the page is not created with our layout editor.</p>
+                    </Alert>
+                )}
+            </BootstrapContainer>
+        </>
     );
 };
 
