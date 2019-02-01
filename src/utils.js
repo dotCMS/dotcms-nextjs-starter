49423d0a15e432ac69627dd9b92a6df00a1e20f4
Alfredo Li <alfredo@dotcms.com>
2019-02-01 12:36:18 -0600

    Remove util not needed

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export const dateFormat = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
-};
