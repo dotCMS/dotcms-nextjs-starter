28d95eaf6ffbec7d3ec57194ffb034512a64cdae
Freddy Montes <freddymontes@gmail.com>
2019-02-04 16:43:26 -0600

    dotCMS/core#15918 Fix DOMException: Node was not found error in edit mode (#24)

--- src/libs/DotContentlet.js
+++ src/libs/DotContentlet.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
 const DotContelet = (props) => {
-    const data = props.data;
     return (
         <div
             data-dot-object="contentlet"
-            data-dot-inode={data.inode} 
-            data-dot-identifier={data.identifier} 
-            data-dot-type={data.contentType}
-            data-dot-basetype={data.dotBasetype}
-            data-dot-lang={data.dotLang}
-            data-dot-title={data.title}
-            data-dot-can-edit={data.dotCanEdit || true}
-            data-dot-content-type-id={data.dotContentTypeId} 
-            // data-dot-has-page-lang-version={data.dotHasPageLangVersion}
-            data-dot-has-page-lang-version="true">
-            {props.children}
+            data-dot-inode={props.inode}
+            data-dot-identifier={props.identifier}
+            data-dot-type={props.contentType}
+            data-dot-basetype={props.dotBasetype}
+            data-dot-lang={props.dotLang}
+            data-dot-title={props.title}
+            data-dot-can-edit={props.dotCanEdit || true}
+            data-dot-content-type-id={props.dotContentTypeId}
+            // data-dot-has-page-lang-version={props.dotHasPageLangVersion}
+            data-dot-has-page-lang-version="true"
+        >
+            {/* This extra <div> is to prevent error in edit mode with the Loadable component */}
+            <div>{props.children}</div>
         </div>
-    )
+    );
 };
 
 export default DotContelet;
