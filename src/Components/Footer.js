0cf15922b1e03013c3156fde9b187a36ae3823ec
Freddy Montes <freddymontes@gmail.com>
2019-02-07 10:05:30 -0600

    Show/hide header and footer, catch no contentlets

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container as BootstrapContainer } from 'reactstrap';
+
+import './Header.css';
+
+const Footer = (props) => {
+    return (
+        <BootstrapContainer>
+            <h2>Footer</h2>
+        </BootstrapContainer>
+    );
+};
+
+export default Footer;
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Container as BootstrapContainer } from 'reactstrap';
 import Header from './Header';
+import Footer from './Footer';
+
+import './Layout.css'
 
 const Layout = (props) => {
+    const contentClass = `layout__content ${!props.header && 'layout__content--no-header'}`
+
     return (
         <>
-            <Header title={props.title} />
-            <BootstrapContainer className="layout__content">{props.children}</BootstrapContainer>
+            {props.header ? <Header title={props.title} /> : null}
+            <BootstrapContainer className={contentClass}>{props.children}</BootstrapContainer>
+            {props.footer ? <Footer /> : null}
         </>
     );
 };
