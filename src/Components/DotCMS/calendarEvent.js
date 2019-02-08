975626100553d16b785f99d7f07a73e7e846eb95
Will Ezell <will@dotcms.com>
2019-02-08 13:13:37 -0500

    asdasd

--- src/Components/DotCMS/calendarEvent.js
+++ src/Components/DotCMS/calendarEvent.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-
-import Moment from 'react-moment';
-
 import DateFormat from '../Shared/DateFormat';
 
 function createMarkup(html) {
@@ -10,20 +7,12 @@ function createMarkup(html) {
 
 export default class calendarEvent extends Component {
     render() {
-        
-        var startDate = new Date(this.props.startDate);
-        var endDate = new Date(this.props.endDate);
-        var desc = (this.props.description + "") ;
-        desc = desc.length>200 ? this.props.description.substring(0,200) : this.props.description
-        desc+="...";
         return (
             <div className="event">
                 <h2>{this.props.title}</h2>
-
-                <time><b><Moment format="MMM, D">{startDate}</Moment></b>: <Moment format="H:mm">{startDate}</Moment>-<Moment format="H:mm">{endDate}</Moment></time>
-                <span dangerouslySetInnerHTML={createMarkup(desc)} />
-
-
+                <DateFormat dateString={this.props.startDate}/>
+                <DateFormat dateString={this.props.endDate}/>
+                <span dangerouslySetInnerHTML={createMarkup(this.props.description)} />
             </div>
         );
     }
