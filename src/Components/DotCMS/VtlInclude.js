4ab93c589f1c6bd1548fb1aa1abf0c54c5648fc2
Will Ezell <will@dotcms.com>
2019-02-08 12:30:25 -0500

    added new package.jspon

--- src/Components/DotCMS/VtlInclude.js
+++ src/Components/DotCMS/VtlInclude.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-import dotcmsApi from '../../libs/dotcms.api';
 import SimpleWidget from './SimpleWidget';
 
 export default class VtlInclude extends SimpleWidget {
