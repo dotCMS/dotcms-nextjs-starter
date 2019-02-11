6f791c89223c5d7ebd50d379c48d0634d98f0df2
Freddy Montes <freddymontes@gmail.com>
2019-02-11 13:56:57 -0600

    Fix issues with header and footer

--- src/libs/dotcms.api.js
+++ src/libs/dotcms.api.js
@@ -28,7 +28,11 @@ const translate = (page) => {
             });
         });
 
-        if (page.layout.sidebar && page.layout.sidebar.containers.length) {
+        if (
+            page.layout.sidebar &&
+            page.layout.sidebar.containers &&
+            page.layout.sidebar.containers.length
+        ) {
             page.layout.sidebar.containers = page.layout.sidebar.containers.map((container) => {
                 const contentlets =
                     page.containers[container.identifier].contentlets[`uuid-${container.uuid}`];
