7fe244179edf0ec455ba7f28bd01aa5bd8b1fd5d
Will Ezell <will@dotcms.com>
2019-02-12 16:29:54 -0500

    added banner

--- src/Components/DotCMS/Banner.js
+++ src/Components/DotCMS/Banner.js
@@ -8,15 +8,17 @@ export default class SimpleWidget extends Component {
 
     componentDidMount() {
         dotcmsApi.request({
-            url: `${process.env.REACT_APP_DOTCMS_HOST}/api/widget/id/` + this.props.identifier
+            url: `${process.env.REACT_APP_DOTCMS_HOST}/api/content/id/` + this.props.identifier
         })
-        .then(response =>  response.text())
-        .then(text => {
+        .then(response =>  response.json())
+        .then(jsonBody => {
             this.setState({
                 ...this.state,
-                widgetCode: text
+                responseJson: jsonBody.contentlets[0],
+                widgetCode:jsonBody.contentlets[0].caption1
             });
- 
+            console.log(this.state.responseJson)
+
         })
     }
     render() {
@@ -30,12 +32,20 @@ export default class SimpleWidget extends Component {
             backgroundSize:"cover"
           };
 
-
+          
 
         return (
+            <div className="bg-wrapper" style={divStyle}>
+                <div className="container">
+                    <div className="row flex-row-reverse">
+                        <div className="col-md-7 col-md-offset-5 " style={{ marginTop: `30px` }}>
+                            <div style={{  background: `rgba(255,255,255,.5)`, textAlign:"center",padding:'10px' }} dangerouslySetInnerHTML={{ __html: this.state.widgetCode }} />
 
-                
-                <div className="bg-wrapper" style={divStyle} />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
  
                 
