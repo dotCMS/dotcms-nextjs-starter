5be0f749be72bc30c0e7e7cfc6ab09f9eecfcd4b
Will Ezell <will@dotcms.com>
2019-02-12 16:11:20 -0500

    added banner

--- src/Components/DotCMS/Banner.js
+++ src/Components/DotCMS/Banner.js
@@ -1,5 +1,45 @@
-import SimpleWidget from './SimpleWidget';
+import React, { Component } from 'react';
+import dotcmsApi from '../../libs/dotcms.api';
 
-export default class Banner extends SimpleWidget {
+export default class SimpleWidget extends Component {
+    state = {
+        widgetCode:'test'
+    };
 
+    componentDidMount() {
+        dotcmsApi.request({
+            url: `${process.env.REACT_APP_DOTCMS_HOST}/api/widget/id/` + this.props.identifier
+        })
+        .then(response =>  response.text())
+        .then(text => {
+            this.setState({
+                ...this.state,
+                widgetCode: text
+            });
+ 
+        })
+    }
+    render() {
+
+        const divStyle = {
+            backgroundColor: '40px',
+            border: '5px solid white',
+            backgroundImage: `url('/dA/${this.props.identifier}/image/1200h/jpegp/retiree-banner.jpeg)`,
+            height: "400px",
+            marginTop:`-150px`,
+            backgroundSize:"cover"
+          };
+
+
+
+        return (
+
+                
+                <div className="bg-wrapper" style={divStyle} />
+
+ 
+                
+
+        );
+    }
 }
