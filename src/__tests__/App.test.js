6f7a9501505994cb81b8eb17fa19e89b9349331a
Alfredo Li <alfredo@dotcms.com>
2019-02-20 13:51:40 -0600

    Add App.js test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import DotCMSApi from '../libs/dotcms.api';
 import { StaticRouter } from 'react-router';
+import wait from 'waait';
 
 import PAGE_MOCK_FORMATTED from '../TestUtils/data';
 
 describe('<App />', () => {
-    it('renders without crashing', () => {
+    beforeEach(() => {
+        DotCMSApi.getConfiguration = jest.fn().mockImplementation(() => {
+            return new Promise((resolve, reject) => {
+                resolve({
+                    status: 200,
+                    json: () =>
+                        new Promise((resolve, reject) => {
+                            resolve();
+                        })
+                });
+            });
+        });
+
+        DotCMSApi.page.setLanguage = jest.fn().mockImplementation(() => {
+            return { code: 'en', id: 1 };
+        });
+
+        DotCMSApi.page.get = jest.fn().mockImplementation(() => new Promise((resolve, reject) => {
+            resolve({...PAGE_MOCK_FORMATTED});
+        }));
+    });
+
+    it('renders with page data (SSR)', () => {
         const context = {};
         const div = document.createElement('div');
         ReactDOM.render(
             <StaticRouter location="someLocation" context={context}>
-                <App data={PAGE_MOCK_FORMATTED.layout} />
+                <App {...PAGE_MOCK_FORMATTED} />
             </StaticRouter>,
             div
         );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders Client Side with page', async () => {
+        const context = {};
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <StaticRouter location="someLocation" context={context}>
+                <App />
+            </StaticRouter>,
+            div
+        );
+        expect(DotCMSApi.getConfiguration).toHaveBeenCalled();
+        await wait();
+        expect(DotCMSApi.page.setLanguage).toHaveBeenCalledWith('someLocation');
+        expect(DotCMSApi.page.get).toHaveBeenCalledWith({pathname: 'someLocation'});
         ReactDOM.unmountComponentAtNode(div);
     });
 });
