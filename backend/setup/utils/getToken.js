4c058059fc2cd6f6d6ab3926d2a674c2c10a70dc
Freddy Montes <freddymontes@gmail.com>
2019-02-27 16:10:21 -0600

    Replace lib in server

--- backend/setup/utils/getToken.js
+++ backend/setup/utils/getToken.js
@@ -1,8 +1,8 @@
-import DotCMSApi from '../../../src/libs/dotcms.api';
+import dotcmsApi from '../../../src/dotcmsApi';
 import { printError } from './index';
 
 export const getToken = ({ user, password, expirationDays, host }) => {
-    return DotCMSApi.auth
+    return dotcmsApi.auth
         .login({ user, password, expirationDays, host })
         .then(res => {
             if (res.status === 200) {
--- src/dotcmsApi.js
+++ src/dotcmsApi.js
@@ -1,10 +1,9 @@
 import { initDotCMS } from 'dotcms';
 
 const dotcms = initDotCMS({
-    host: 'http://localhost:8080',
-    environment: 'development',
-    token:
-        'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1MGEwNDk0MC01NmEzLTQ1MGQtYWU5Ny0xY2YyY2M2NGUzMTgiLCJpYXQiOjE1NTEyOTI2ODQsInVwZGF0ZWRfYXQiOjEyMDQ4MjQ5NjEwMDAsInN1YiI6ImRvdGNtcy5vcmcuMSIsImlzcyI6IjU2MDk4YTIyLTgzYjEtNDZmMS05ZjZiLWYwMTRmOWI2NjRiMyIsImV4cCI6MTU1MjE1NjY4NH0.U04A_TU4LTKJvLhkLpJkhvCnqwgKFGtsITsZSPDSkpc'
+    host: process.env.REACT_APP_DOTCMS_HOST,
+    environment: 'production',
+    token: process.env.REACT_APP_BEARER_TOKEN
 });
 
 export default dotcms;
