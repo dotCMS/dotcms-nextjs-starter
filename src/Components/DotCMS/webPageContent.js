4989e127b3d12b635dfbaa15d4bc1ece568f962d
Will Ezell <will@dotcms.com>
2019-10-29 10:47:03 -0400

    better looking spa

--- src/Components/DotCMS/webPageContent.js
+++ src/Components/DotCMS/webPageContent.js
@@ -8,7 +8,7 @@ export default class webPageContent extends Component {
     render() {
         return (
             <div className="content">
-                <h2>{this.props.title}</h2>
+
                 <span dangerouslySetInnerHTML={createMarkup(this.props.body)} />
             </div>
         );
