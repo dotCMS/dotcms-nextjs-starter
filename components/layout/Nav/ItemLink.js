831a070d29b494ffea05c4f89421612d63474f4d
Alfredo Li <alfredo@dotcms.com>
2019-11-13 18:35:54 -0600

    progress

--- components/layout/Nav/ItemLink.js
+++ components/layout/Nav/ItemLink.js
@@ -1,18 +1,33 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+const getLink = (props, p) => (
+  <a href={p.href} className={props.className}>
+    {props.children}
+  </a>
+);
+
+const getList = (props, p) => (
+  <li
+    className={props.topMenu && props.dropDown ? props.className : p.className}
+  >
+    {!Array.isArray(props.children) ? getLink(props, p) : <>{props.children}</>}
+  </li>
+);
 
 const ItemLink = props => {
-    return (
-        <NavLink
-            activeClassName="active"
-            className="rd-nav-item"
-            to={{
-                pathname: props.pathname,
-                state: props.state
-            }}
-        >
-            <span className={props.className}>{props.children}</span>
-        </NavLink>
-    );
+  return (
+    <NavLink
+      component={p => {
+        return props.topMenu ? getList(props, p) : getLink(props, p);
+      }}
+      activeClassName="active"
+      className="rd-nav-item"
+      to={{
+        pathname: props.pathname,
+        state: props.state
+      }}
+    />
+  );
 };
 
 export default ItemLink;
--- components/layout/Nav/NavDropDown.js
+++ components/layout/Nav/NavDropDown.js
@@ -1,66 +1,81 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import ItemLink from './ItemLink';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import ItemLink from "./ItemLink";
 
 const NavDropDown = ({ options }) => {
-    const [focus, setFocus] = useState(false);
+  const [focus, setFocus] = useState(false);
 
-    const classStatus = `rd-nav-item rd-navbar--has-megamenu rd-navbar-submenu ${focus ? 'focus' : ''}`;
+  const classStatus = `rd-nav-item rd-navbar--has-megamenu rd-navbar-submenu ${
+    focus ? "focus" : ""
+  }`;
 
-    return (
-        <li
-            className={classStatus}
-            onMouseOver={() => {
-                setFocus(true);
-            }}
-            onMouseOut={() => {
-                setFocus(false);
-            }}
-        >
-            <ItemLink pathname={options.href} className="rd-nav-link">
-                {options.title}
-            </ItemLink>
-            <span className="rd-navbar-submenu-toggle"></span>
+  return (
+    <ItemLink
+      pathname={options.href}
+      className={classStatus}
+      dropDown="true"
+      topMenu="true"
+      onMouseOut={() => {
+        setFocus(false);
+      }}
+    >
+      <a
+        href={options.href}
+        className="rd-nav-link"
+        onMouseOver={() => {
+          setFocus(true);
+        }}
+        
+      >
+        {options.title}
+      </a>
+      <span className="rd-navbar-submenu-toggle"></span>
 
-            <ul className="rd-menu rd-navbar-megamenu rd-navbar-open-left rd-navbar-open-right">
-                {options.children.map((subItem, index) => {
-                    return (
-                        <li className="rd-megamenu-item" key={index}>
-                            <div className="rd-megamenu-title">
-                                <ItemLink pathname={subItem.href} target={subItem.target}>
-                                    {subItem.title}
-                                </ItemLink>
-                            </div>
-                            <ul className="rd-megamenu-list">
-                                {subItem.children.map((extraItem, k) => {
-                                    return (
-                                        <li className="rd-megamenu-list-item" key={k}>
-                                            <ItemLink pathname={extraItem.href} target={extraItem.target}>
-                                                {extraItem.title}
-                                            </ItemLink>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </li>
-                    );
+      <ul onMouseOver={() => {
+          setFocus(true);
+        }} className="rd-menu rd-navbar-megamenu rd-navbar-open-left rd-navbar-open-right">
+        {options.children.map((subItem, index) => {
+          return (
+            <li className="rd-megamenu-item" key={index}>
+              <div className="rd-megamenu-title">
+                <ItemLink pathname={subItem.href} target={subItem.target}>
+                  {subItem.title}
+                </ItemLink>
+              </div>
+              <ul className="rd-megamenu-list">
+                {subItem.children.map((extraItem, k) => {
+                  return (
+                    <li className="rd-megamenu-list-item" key={k}>
+                      <ItemLink
+                        className="rd-dropdown-link"
+                        pathname={extraItem.href}
+                        target={extraItem.target}
+                      >
+                        {extraItem.title}
+                      </ItemLink>
+                    </li>
+                  );
                 })}
-            </ul>
-        </li>
-    );
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </ItemLink>
+  );
 };
 
 NavDropDown.propTypes = {
-    options: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        children: PropTypes.arrayOf(
-            PropTypes.shape({
-                folder: PropTypes.string,
-                href: PropTypes.string.isRequired,
-                title: PropTypes.string.isRequired
-            })
-        )
-    }).isRequired
+  options: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    children: PropTypes.arrayOf(
+      PropTypes.shape({
+        folder: PropTypes.string,
+        href: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired
+      })
+    )
+  }).isRequired
 };
 
 export default NavDropDown;
