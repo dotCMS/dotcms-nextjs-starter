f3cbda3a7411734b35908ab4de70aa04db012d25
Humberto Morera <humberto.morera@dotcms.com>
2019-11-14 15:09:20 -0600

    revert itemLink

--- components/layout/Nav/ItemLink.js
+++ components/layout/Nav/ItemLink.js
@@ -1,22 +1,16 @@
 import { Link } from 'react-router-dom';
-import {PageContext} from "../../../pages/dotcms";
 
 const ItemLink = props => {
     return (
-        <PageContext.Consumer>
-            {({ lang }) => (
-                <Link
-                    className={props.className}
-                    to={{
-                        pathname: props.pathname,
-                        state: props.state,
-                        search: `?lang=${lang}`
-                    }}
-                >
-                    {props.children}
-                </Link>
-            )}
-        </PageContext.Consumer>
+        <Link
+            className={props.className}
+            to={{
+                pathname: props.pathname,
+                state: props.state
+            }}
+        >
+            {props.children}
+        </Link>
     );
 };
 
