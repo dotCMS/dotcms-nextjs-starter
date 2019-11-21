a15cf13d1feef36f7dac395cf7f80dd67cb86ea8
Humberto Morera <humberto.morera@dotcms.com>
2019-11-21 13:40:18 -0600

    add temp styles and add prettier congif as core-web

--- components/layout/LocaleDropdown.js
+++ components/layout/LocaleDropdown.js
@@ -1,24 +1,38 @@
-import { PageContext } from "../../pages/_app";
-import React from "react";
+import { PageContext } from '../../pages/_app';
+import React from 'react';
 
 const Options = ({ languages }) => {
-  return languages.map(lang => (
-    <option key={lang.languageCode} value={lang.id}>
-      {lang.language}
-    </option>
-  ));
+    return languages.map(lang => (
+        <option key={lang.languageCode} value={lang.id}>
+            {lang.language}
+        </option>
+    ));
 };
 
 const LocaleDropdown = () => {
-  return (
-    <PageContext.Consumer>
-      {({ language }) => (
-        <select defaultValue={language.current} onChange={language.set}>
-          <Options languages={language.options} />
-        </select>
-      )}
-    </PageContext.Consumer>
-  );
+    //TODO: Remove this after work on select component.
+    const tempStyle = {
+        minHeight: '56px',
+        padding: '15px 22px',
+        backgroundColor: 'transparent',
+        appearance: 'none',
+        WebkitAppearance: 'none',
+        borderRadius: '0',
+        border:'0',
+        outline:'none',
+        borderLeft: '1px solid #aeb1be',
+        borderRight: '1px solid #aeb1be'
+    };
+
+    return (
+        <PageContext.Consumer>
+            {({ language }) => (
+                <select style={tempStyle} defaultValue={language.current} onChange={language.set}>
+                    <Options languages={language.options} />
+                </select>
+            )}
+        </PageContext.Consumer>
+    );
 };
 
 export default LocaleDropdown;
