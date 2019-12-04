e1dba962f22d1e05ef950e9dc3ab2d7e0715de0b
Freddy Montes <freddymontes@gmail.com>
2019-12-04 14:45:27 -0600

    Remove duplicated title in the web page content

--- components/content-types/webPageContent.js
+++ components/content-types/webPageContent.js
@@ -3,10 +3,5 @@ function createMarkup(html) {
 }
 
 export default function webPageContent(props) {
-    return (
-        <div className="content">
-            <h2>{props.title}</h2>
-            <span dangerouslySetInnerHTML={createMarkup(props.body)} />
-        </div>
-    );
+    return <div className="content" dangerouslySetInnerHTML={createMarkup(props.body)}></div>;
 }
