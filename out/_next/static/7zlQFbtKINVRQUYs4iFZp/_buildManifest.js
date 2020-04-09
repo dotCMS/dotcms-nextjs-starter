14f70174bd833f3de7a3dd505c1231b2cabbfcda
Freddy Montes <freddymontes@gmail.com>
2020-04-09 14:08:34 -0600

    Deploy gh-pages

---
+++ out/_next/static/7zlQFbtKINVRQUYs4iFZp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/[...slug]":[a],"/_error":[a]}}("static\u002Fchunks\u002F87385f066fc4158ef99c3b2603860a8022f72c2b.b2e169c2268de7621e21.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/_next/static/nBWMTEJmktvjXWwdmfFJ4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/[...slug]":[a],"/_error":[a]}}("static\u002Fchunks\u002Fe63faeb82f83a6f615f34658be439087e52631d2.b2e169c2268de7621e21.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
