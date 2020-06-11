13e6317870fc465af692fe084d3ac5eb6602a969
Freddy Montes <freddymontes@gmail.com>
2020-06-11 09:05:28 -0600

    Fix RouterLink

--- components/RouterLink.js
+++ components/RouterLink.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 
+const slugMap = {
+    '/store/category': '/store/category/[slug]',
+    '/store/products': '/store/[[...slug]]',
+    '': '/store/[[...slug]]'
+};
+
 const RouterLink = ({ href, children, className }) => {
+    const slug = href.split('/').slice(0, -1).join('/');
+    const file = slugMap[slug];
     return (
-        <Link href="/[...slug]" as={`${href}`}>
+        <Link href={file} as={`${href}`}>
             <a className={className}>{children}</a>
         </Link>
     );
