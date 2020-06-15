c9ec2ae7bf173c139b42fb18fedb223447f8ba5d
Freddy Montes <freddymontes@gmail.com>
2020-06-15 07:51:42 -0600

    Update EMA server

--- server/server.js
+++ server/server.js
@@ -6,10 +6,8 @@ const bodyParser = require('body-parser');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
 
 const { loggerLog } = require('../utilities/logger');
-const { isNextInternalFile } = require('./next');
 const dotcms = require('../config/dotcms');
 
 const formUrlEncodedParser = bodyParser.raw({
@@ -22,75 +20,9 @@ app.prepare()
     .then(() => {
         const server = express();
 
-        server.get('*', async (req, res, _next) => {
-            try {
-                const url = req.path;
-                if (dotcms.isPage(url) || isNextInternalFile(url)) {
-                    return handle(req, res);
-                } else {
-                    return dotcms.proxyToStaticFile(req, res, next);
-                }
-            } catch (error) {
-                /*
-                    If the request to DotCMS fail we have a fallback chain in place.
-                */
-                switch (error.statusCode) {
-                    /*
-                        If the page doesn't have a layout we render the error using next right away.
-                    */
-                    case dotcms.errors.DOTCMS_NO_LAYOUT:
-                        res.statusCode = 406; // Not Acceptable
-                        error.statusCode = res.statusCode;
-                        error.traceError = error.stack;
-
-                        app.renderError(null, req, res, req.path, {
-                            customError: error
-                        });
-                        break;
-
-                    case dotcms.errors.DOTCMS_CUSTOM_ERROR:
-                        res.statusCode = 406; // Not Acceptable
-                        error.statusCode = 500;
-                        error.traceError = error.stack;
-
-                        app.renderError(null, req, res, req.path, {
-                            customError: error
-                        });
-                        break;
-                    /*
-                        But if the request to DotCMS fail because the instance is down or auth
-                        wasn't sucessuful, we try to render the page using next static page feature.
-
-                        If the page exist in next all good but if not next will render a 404.
-
-                        Also we are setting in dev mode a dotcmsStatus message that we will render
-                        in all the pages just in edit mode to tell developers that something is up
-                        with the DotCMS instance they are trying to reach.
-                    */
-                    case dotcms.errors.DOTCMS_DOWN:
-                    case dotcms.errors.DOTCMS_NO_AUTH:
-                        let dotcmsStatus;
-
-                        if (dev) {
-                            dotcmsStatus = error.message;
-                        }
-
-                        app.render(req, res, req.path, { dotcmsStatus });
-                        break;
-                    default:
-                        app.renderError(null, req, res, req.path, {
-                            customError: error
-                        });
-                }
-            }
-        });
-
-        /*
-            We can assume (at least for now) that all requests to /api/* are meant
-            to DotCMS instance, so we just proxy them.
-        */
-        server.post('/api/*', async (req, res) => dotcms.proxyToStaticFile(req, res));
-        server.put('/api/*', async (req, res) => dotcms.proxyToStaticFile(req, res));
+        server.get('*', (req, res) => {
+            res.send('DotCMS Edit Mode Server');
+        })
 
         /*
             DotCMS Edit Mode Anywhere Plugin works by sending a POST request to the configured
@@ -105,10 +37,10 @@ app.prepare()
             const page = JSON.parse(querystring.parse(req.body.toString()).dotPageData).entity;
             const pageRender = await dotcms.transformPage(page);
             app.setAssetPrefix(process.env.PUBLIC_URL);
-            app.render(req, res, '/dotcms', { pageRender, isBeingEditFromDotCMS: true });
+            app.render(req, res, req.path, { pageRender, isBeingEditFromDotCMS: true });
         });
 
-        server.listen(3000, (err) => {
+        server.listen(5000, (err) => {
             if (err) throw err;
             console.log(`> Ready on ${process.env.PUBLIC_URL}`);
         });
