ef42acb4afaed32249062b1c9b29d5af19029832
Ivor Padilla <ivorjpc@gmail.com>
2020-06-22 21:30:11 +0200

    Fixed typo

--- hocs/withApollo.js
+++ hocs/withApollo.js
@@ -7,7 +7,6 @@ const cache = new InMemoryCache();
 export default withApollo(
     ({ initialState }) => {
         return new ApolloClient({
-            uri: 'https://starter.dotcms.com:8443/api/v1/graphql',
             uri: `${process.env.NEXT_PUBLIC_DOTCMS_HOST}/api/v1/graphql`,
             cache: cache.restore(initialState || {})
         });
