9ea41e865c09d0cc63139702e7802b513dade882
Ivor Padilla <ivorjpc@gmail.com>
2020-06-22 20:07:30 +0200

    clean up

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 const withCss = require('@zeit/next-css');
-const Dotenv = require('dotenv-webpack');
+// const Dotenv = require('dotenv-webpack');
 
 module.exports = withCss({
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                }
-            }
-        });
-        config.plugins.push(new Dotenv({ silent: false }));
-        return config;
-    },
     experimental: {
         optionalCatchAll: true
     }
