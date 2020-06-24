1d17c34f59a1493a3c9354e39c491792735ef93f
Freddy Montes <freddymontes@gmail.com>
2020-06-24 15:52:20 -0600

    Update RouterLink

--- components/RouterLink.js
+++ components/RouterLink.js
@@ -1,16 +1,11 @@
 import Link from 'next/link';
 
-const slugMap = {
-    '/store/category': '/store/category/[slug]',
-    '/store/products': '/store/[[...slug]]',
-    '': '/store/[[...slug]]'
-};
-
 const RouterLink = ({ href, children, className, ariaLabel }) => {
-    const slug = href.split('/').slice(0, -1).join('/');
-    const file = slugMap[slug];
     return (
-        <Link href={file} as={`${href}`}>
+        <Link
+            href={href.includes('/store/category') ? '/store/category/[slug]' : '/[...slug]'}
+            as={`${href}`}
+        >
             <a aria-label={ariaLabel} className={className}>
                 {children}
             </a>
