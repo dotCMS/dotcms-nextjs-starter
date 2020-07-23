5867acbb3f8d26c57d7cfda647648fc914c3bddb
Freddy Montes <freddymontes@gmail.com>
2020-07-23 15:56:50 -0600

    Style Activities

--- components/content-types/rootContentTypes.js
+++ components/content-types/rootContentTypes.js
@@ -1,7 +1,9 @@
 import dynamic from 'next/dynamic';
+
+export const Activity = dynamic(() => import('../content-types/Activity'));
 export const Banner = dynamic(() => import('../content-types/Banner'));
+export const CategoryFilter = dynamic(() => import('../content-types/CategoryFilter'));
+export const ProductDetail = dynamic(() => import('./ProductDetail'));
 export const SimpleWidget = dynamic(() => import('../content-types/SimpleWidget'));
 export const StoreProductList = dynamic(() => import('../content-types/StoreProductList'));
 export const webPageContent = dynamic(() => import('../content-types/webPageContent'));
-export const ProductDetail = dynamic(() => import('./ProductDetail'));
-export const CategoryFilter = dynamic(() => import('../content-types/CategoryFilter'));
