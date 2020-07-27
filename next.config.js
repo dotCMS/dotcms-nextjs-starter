af5e7b0133879d703bd16f6fa0eee45f35f62be6
Freddy Montes <freddymontes@gmail.com>
2020-07-27 13:27:05 -0600

    Update next to 9.5 and use rewrite to proxy the images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withCss = require('@zeit/next-css');
 
 module.exports = withCss({
+    async rewrites() {
+        return [
+            // check if Next.js project routes match before we attempt proxying
+            {
+                source: '/dA/:slug*',
+                destination: `https://starter.dotcms.com:8443/dA/:slug*`
+            }
+        ];
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
