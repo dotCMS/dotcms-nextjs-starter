df741f72fe10812a63c0ac22f7befcd7a95e2203
Freddy Montes <freddymontes@gmail.com>
2020-07-30 15:50:50 -0600

    Fix key error in dev mode

--- utilities/dotcms/htmlParser.js
+++ utilities/dotcms/htmlParser.js
@@ -2,15 +2,17 @@ import DotCMSImage from '../../components/DotCMSImage';
 import RouterLink from '../../components/RouterLink';
 import ReactHtmlParser, { processNodes } from 'react-html-parser';
 
-const transform = (node, _index) => {
+const transform = (node, index) => {
     if (node.type === 'tag') {
         if (node.name === 'img') {
-            return <DotCMSImage {...node.attribs} />;
+            return <DotCMSImage key={index} {...node.attribs} />;
         }
 
         if (node.name === 'a') {
             return (
-                <RouterLink {...node.attribs}>{processNodes(node.children, transform)}</RouterLink>
+                <RouterLink key={index} {...node.attribs}>
+                    {processNodes(node.children, transform)}
+                </RouterLink>
             );
         }
     }
