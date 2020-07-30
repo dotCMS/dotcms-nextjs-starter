9ba9949d3f1c83691a4c9285a39f7d4e8fcefa12
Ivor Padilla <ivorjpc@gmail.com>
2020-07-30 21:21:44 +0200

    Moved parser to helper function

---
+++ components/content-types/parseContent.js
@@ -0,0 +1,21 @@
+import DotCMSImage from '../DotCMSImage';
+import RouterLink from '../RouterLink';
+import ReactHtmlParser, { processNodes } from 'react-html-parser';
+
+const transform = (node, _index) => {
+    if (node.type === 'tag') {
+        if (node.name === 'img') {
+            return <DotCMSImage {...node.attribs} />;
+        }
+
+        if (node.name === 'a') {
+            return (
+                <RouterLink {...node.attribs}>{processNodes(node.children, transform)}</RouterLink>
+            );
+        }
+    }
+};
+
+export default function withRawContent({ content }) {
+    return ReactHtmlParser(content, { transform });
+}
