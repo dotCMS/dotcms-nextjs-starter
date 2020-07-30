a2fa14e86a27345c57d05992ba7a380b48214dc6
Freddy Montes <freddymontes@gmail.com>
2020-07-30 16:58:41 -0600

    Use getLanguagesProps

--- utilities/dotcms/getLanguages.js
+++
@@ -1,7 +0,0 @@
-const dotCMSApi = require('../../config/dotcmsApi');
-
-const getLanguages = () => {
-    return dotCMSApi.language.getLanguages();
-};
-
-module.exports = getLanguages
--- utilities/dotcms/index.js
+++ utilities/dotcms/index.js
@@ -1,12 +1,9 @@
-
 const CustomError = require('../custom-error');
 const dotCMSApi = require('../../config/dotcmsApi');
-const getLanguages = require('./getLanguages');
+const getLanguagesProps = require('./getLanguagesProps');
 const getPage = require('./getPage');
 const getPathsArray = require('./getPathsArray');
 const getNav = require('./getNav');
-
-
 
 function emitRemoteRenderEdit(url) {
     console.log('emitting event');
@@ -16,51 +13,11 @@ function emitRemoteRenderEdit(url) {
     });
 }
 
-
-const getLanguagesProps = async (selectedLanguage = '') => {
-    // Fetch list of languages supported in the DotCMS instance so we can inject the data into the static pages
-    // and map to a clean array of ISO compatible lang codes.
-    const languages = await getLanguages();
-
-    // This will be coming from the API
-    const __DEFAULT_LANGUAGE__ = 'en';
-
-    // Returns either true or false if `selectedLanguage` in a valid language from our languages array
-    let hasLanguages = languages
-        .map((language) => language.languageCode)
-        .filter((language) => language !== __DEFAULT_LANGUAGE__)
-        .includes(selectedLanguage);
-
-    // If the hasLanguages predicate returns true find the language in the languages array and pass it in `getPage` call
-    const languageId = hasLanguages
-        ? languages.find((lang) => lang.languageCode === selectedLanguage).id
-        : '1';
-
-    return new Promise((resolve) => {
-        let results = {
-            hasLanguages,
-            languageId,
-            languages,
-            defaultLanguage: __DEFAULT_LANGUAGE__
-        };
-
-        resolve(
-            hasLanguages
-                ? {
-                      ...results,
-                      selectedLanguage
-                  }
-                : results
-        );
-    });
-};
-
 module.exports = {
     CustomError,
     getPage,
     getNav,
     emitRemoteRenderEdit,
     getPathsArray,
-    getLanguages,
     getLanguagesProps
 };
