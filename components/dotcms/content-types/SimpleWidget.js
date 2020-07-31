b711e14c6a5eac54efd3b672becd77ad4573dd19
Ivor Padilla <ivorjpc@gmail.com>
2020-07-31 18:29:09 +0200

    Improvement: Targeting components with styled-components and DotCMS contentlet identifier Bugfix: Overflow issue in video container

--- components/dotcms/content-types/SimpleWidget.js
+++ components/dotcms/content-types/SimpleWidget.js
@@ -1,6 +1,20 @@
 import htmlParser from '../../../utilities/htmlParser';
+import styled, { css } from 'styled-components';
 
-export default function SimpleWidget({ rendered }) {
-    return htmlParser({ content: rendered });
+const FormBooking = css`
+    /* background: red;
+    padding: 3rem;
+    margin-bottom: 3rem; */
+`;
+
+const identifierMap = {
+    'df591adf-10fe-461a-a12e-f847df0fd2fb': FormBooking
+};
+
+const WidgetContainer = styled.div`
+    ${(props) => identifierMap[props.identifier] || null};
+`;
+
+export default function SimpleWidget({ rendered, identifier }) {
+    return <WidgetContainer identifier={identifier}>{htmlParser({ content: rendered })}</WidgetContainer>;
 }
-
