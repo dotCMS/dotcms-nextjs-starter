7e9acf8b472c048b550271a5b5ee600898aa45a3
Daniel Esteves <estevesd8@gmail.com>
2021-08-26 18:28:49 -0400

    fix: environment variables pointing to vercel_url

--- src/components/LocalImage.tsx
+++ src/components/LocalImage.tsx
@@ -4,11 +4,7 @@ import type { ImageProps } from 'next/image'
 
 export const LocalImage = (props: ImageProps) => {
   const myLoader = ({ src }: { src: string }) => {
-    const currentDeployURL = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : process.env.NEXT_PUBLIC_DEPLOY_URL
-
-    return `${currentDeployURL}${src}`
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${src}`
   }
 
   return <Image {...props} alt={props.alt} loader={myLoader} />
