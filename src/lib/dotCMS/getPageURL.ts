8310fa2f83e02715c7d63cd86809ec1dd8779411
Daniel Esteves <estevesd8@gmail.com>
2021-09-09 12:52:15 -0400

    fix: using join instead of string

--- src/lib/dotCMS/getPageURL.ts
+++ src/lib/dotCMS/getPageURL.ts
@@ -4,7 +4,7 @@
 export const getPageUrl = async (slug: string[]): Promise<string> => {
   let category: string[]
   // We need to add "/" before the slug so the page can respond with the HTML
-  let url = slug ? `/${String(slug)}` : '/'
+  let url = slug ? `/${slug.join('/')}` : '/'
 
   // If slug includes the term `category` then we're in a category and we need to build our URL
   if (slug?.includes('category')) {
