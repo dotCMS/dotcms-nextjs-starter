957227062a5f4551486b5b46b1c8d1d79a864b2f
Daniel Esteves <estevesd8@gmail.com>
2021-09-15 10:01:27 -0400

    fix: removing unused code

--- src/components/dotCMS/content-types/CategoryFilter.tsx
+++ src/components/dotCMS/content-types/CategoryFilter.tsx
@@ -16,21 +16,6 @@ export const CategoryFilter = (props: CategoryFilterProps) => {
     (nav) => nav.href === '/Store' || nav.href === '/store'
   )
 
-  // TODO: This reduce is not returning anything, figure out why
-  // const data: any[] = storeNav.children.reduce(function (acc: any, curr: any) {
-  //   if (curr.children.length) {
-  //     acc = [
-  //       ...acc,
-  //       ...curr.children.map((children: { title: string; href: string }) => ({
-  //         title: children.title,
-  //         href: children.href,
-  //       })),
-  //     ]
-  //   }
-
-  //   return acc
-  // }, [])
-
   return (
     <SidebarContainer>
       <h4 className="sidebar-title">{props.title}</h4>
