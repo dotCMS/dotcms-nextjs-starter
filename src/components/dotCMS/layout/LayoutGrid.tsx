4df9aa2b843a60c05e4a236a0170883d70d032b0
Daniel Esteves <estevesd8@gmail.com>
2021-09-17 15:13:27 -0400

    feat: add comments for layout grid

--- src/components/dotCMS/layout/LayoutGrid.tsx
+++ src/components/dotCMS/layout/LayoutGrid.tsx
@@ -23,16 +23,30 @@ export const LayoutGrid = () => {
   const containersData = containers
 
   return body.rows.map(({ columns }, i) => (
+    /**
+     * We loop through the rows and render the columns
+     */
     <Row key={`row-${i}`}>
       {columns.map(({ leftOffset, width, containers }, k) => (
+        /**
+         * - We loop through the columns and render the containers
+         * - We use the leftOffset to calculate the offset of the column
+         * - We use the width to calculate the width of the column
+         */
         <Column key={`col-${k}`} leftOffset={leftOffset} width={width}>
           {containers.map(({ identifier, uuid }, l) => {
+            /**
+             * - We use the uuid to find the contentlets in the containers
+             */
             const contentlets =
               containersData[identifier].contentlets[`uuid-${uuid}`]
 
             return (
               <div key={`container-${l}`}>
                 {contentlets.map((contentlet, m) => {
+                  /**
+                   * - We loop through the contentlets and render the contentlet
+                   */
                   return (
                     <Contentlet data={contentlet} key={`contentlet-${m}`} />
                   )
