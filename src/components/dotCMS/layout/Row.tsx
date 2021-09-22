0cd544be5489b9649c10b32a2cad6d6b8f5fed70
Daniel Esteves <estevesd8@gmail.com>
2021-09-22 12:40:36 -0400

    fix: container for rows

--- src/components/dotCMS/layout/Row.tsx
+++ src/components/dotCMS/layout/Row.tsx
@@ -1,5 +1,9 @@
 export const Row: React.FC = ({ children }): JSX.Element => {
-  return <section className="grid grid-cols-12 gap-2 my-2">{children}</section>
+  return (
+    <section className="container grid grid-cols-12 gap-2 my-2">
+      {children}
+    </section>
+  )
 }
 
 export default Row
