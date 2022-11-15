b861667f3bd0a3034a995a8344b9e374e8edceac
Rafael <rjvelazco21@gmail.com>
2022-11-15 12:07:37 -0400

    dev: show images in containers

--- src/components/dotCMS/content-types/Image/index.tsx
+++ src/components/dotCMS/content-types/Image/index.tsx
@@ -6,20 +6,39 @@ export type ImageProps = {
   title: string
   description: string
   fileAsset: string
+  identifier: string
+  name: string
+  width: number
+  height: number
 }
 
 export const Image = (props: ImageProps) => {
-  const description =
-    props.description.length > 120
-      ? props.description.substring(0, 120)
-      : props.description
+  const { description, fileAsset, title, identifier, name, width, height } =
+    props
+  const imageDescription =
+    description.length > 120 ? description.substring(0, 120) : description;
+  const imagePath = identifier && name ? `/dA/${identifier}/${name}` : fileAsset;
 
   return (
     <Wrapper>
-      <DotCMSImage alt={props.title} path={props.fileAsset} />
+        {width && height ? (
+          <DotCMSImage
+            alt={title}
+            height={height}
+            path={imagePath}
+            width={width}
+          />
+        ) : (
+          // If 'width' or 'height' is not provided, we use a normal img tag.
+          // This code has been extracted from the official documentation: https://nextjs.org/docs/messages/no-img-element#possible-ways-to-fix-it
+          <picture>
+            <source srcSet={imagePath} type="image/webp" />
+            <img alt={title} src={imagePath} />
+          </picture>
+        )}
       <div className="body">
-        <h4>{props.title}</h4>
-        <p>{description}</p>
+        <h4>{title}</h4>
+        <p>{imageDescription}</p>
       </div>
       <div className="overlay" />
     </Wrapper>
